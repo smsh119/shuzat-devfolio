59b67efa50996b12432199740f6f54bf9f38c796
smsh119 <suzathoq16@gmail.com>
2025-10-22 02:12:37 +0600

    Added Button component, implemented Hero and About section markup

---
+++ components/About/TechnologiesAndTools.jsx
@@ -0,0 +1,50 @@
+import ToolBadge from "./ToolBadge";
+
+function TechnologiesAndTools() {
+  const langsAndFrameworks = [
+    { logo: "/images/logos/html5.svg", name: "HTML5" },
+    { logo: "/images/logos/css3.svg", name: "CSS3" },
+    { logo: "/images/logos/javascript.svg", name: "JavaScript" },
+    { logo: "/images/logos/react.svg", name: "ReactJS" },
+    { logo: "/images/logos/nextjs.svg", name: "Next.js" },
+    { logo: "/images/logos/tailwind.svg", name: "Tailwind" },
+    { logo: "/images/logos/nodejs.png", name: "Node.js" },
+    { logo: "/images/logos/express-js.svg", name: "Express.js" },
+  ];
+
+  const tools = [
+    { logo: "/images/logos/vscode.svg", name: "VS Code" },
+    { logo: "/images/logos/github.svg", name: "Github" },
+    { logo: "/images/logos/git.svg", name: "Git" },
+    { logo: "/images/logos/postman.png", name: "Postman" },
+  ];
+
+  return (
+    <>
+      <h2 className="border-text m-auto mb-8 inline-block border-b-2 px-6 text-lg sm:mb-11 sm:text-2xl">
+        Technologies and Tools I Use
+      </h2>
+      <section className="grid grid-cols-1 justify-items-center lg:grid-cols-[1fr_2fr] lg:gap-x-4">
+        <h3 className="mb-10 text-nowrap text-base underline sm:text-xl lg:justify-self-end lg:text-left lg:no-underline lg:after:content-[':']">
+          Languages and Frameworks{" "}
+        </h3>
+        <div className="lg:justify-items-normal mb-12 grid w-2/3 max-w-[324px] grid-cols-3 justify-items-center gap-x-2 gap-y-3 sm:max-w-[540px] sm:gap-x-4 lg:w-11/12 lg:max-w-max lg:grid-cols-4 lg:gap-x-3 lg:justify-self-start">
+          {langsAndFrameworks.map((item) => (
+            <ToolBadge key={item.name} name={item.name} logo={item.logo} />
+          ))}
+        </div>
+
+        <h3 className="mb-10 text-nowrap text-base underline sm:text-xl lg:justify-self-end lg:text-left lg:no-underline lg:after:content-[':']">
+          Tools{" "}
+        </h3>
+        <div className="mb-12 grid grid-cols-2 gap-x-2 gap-y-3 sm:gap-x-12 lg:grid-cols-4 lg:gap-x-3 lg:justify-self-start">
+          {tools.map((item) => (
+            <ToolBadge key={item.name} name={item.name} logo={item.logo} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default TechnologiesAndTools;
---
+++ components/Button.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+
+function Button({
+  children,
+  type = "button",
+  color = "primary",
+  size = "sm",
+  ...rest
+}) {
+  let customClass = "";
+  // add style based on type prop
+  if (color === "primary") customClass = "bg-primary";
+  else if (color === "accent") customClass = "bg-accent";
+
+  // add style based on size prop
+  if (size === "lg")
+    customClass = customClass + " sm:px-8 sm:py-4 sm:text-base";
+
+  if (type === "anchor") {
+    return (
+      <a
+        {...rest}
+        className={`${customClass} active:bg-bg hover:ring-text hover:ring-3 rounded-3xl px-4 py-2 text-xs font-bold transition duration-200 ease-linear`}
+      >
+        {children}
+      </a>
+    );
+  } else if (type === "link") {
+    return (
+      <Link
+        {...rest}
+        className={`${customClass} active:bg-bg hover:ring-text hover:ring-3 rounded-3xl px-4 py-2 text-xs font-bold transition duration-200 ease-linear`}
+      >
+        {children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        {...rest}
+        className={`${customClass} active:bg-bg hover:ring-text hover:ring-3 rounded-3xl px-4 py-2 text-xs font-bold transition duration-200 ease-linear`}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
+export default Button;
