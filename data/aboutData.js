cc854963a57e4119f27ae87afebc2ca79b62fd1b
smsh119 <suzathoq16@gmail.com>
2025-10-23 23:00:04 +0600

    separated data from markup and logic, created data folder and files

---
+++ data/aboutData.js
@@ -0,0 +1,30 @@
+const langsAndFrameworks = [
+  { logo: "/images/logos/html5.svg", name: "HTML5" },
+  { logo: "/images/logos/css3.svg", name: "CSS3" },
+  { logo: "/images/logos/javascript.svg", name: "JavaScript" },
+  { logo: "/images/logos/react.svg", name: "ReactJS" },
+  { logo: "/images/logos/nextjs.svg", name: "Next.js" },
+  { logo: "/images/logos/tailwind.svg", name: "Tailwind" },
+  { logo: "/images/logos/nodejs.png", name: "Node.js" },
+  { logo: "/images/logos/express-js.svg", name: "Express.js" },
+];
+
+const tools = [
+  { logo: "/images/logos/vscode.svg", name: "VS Code" },
+  { logo: "/images/logos/github.svg", name: "Github" },
+  { logo: "/images/logos/git.svg", name: "Git" },
+  { logo: "/images/logos/postman.png", name: "Postman" },
+];
+
+const education = {
+  degree: "BSc. in Information and Communication Engineering",
+  institution: "Uninversity of Rajshahi",
+  country: "Bangladesh",
+};
+
+const interests =
+  "Problem Solving, Learning New Technologies, Building Side Projects, Game Development, Graphic Design, Figma Prototyping, Science related stuff.";
+
+const hobbies = "Traveling, Bike Riding, Gaming, Listening to Music.";
+
+export { education, hobbies, interests, langsAndFrameworks, tools };
---
+++ data/projectsData.js
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    id: "001",
+    projectName: "Project Name",
+    projectDescription:
+      "This is a front-end of a website built with raw HTML and CSS. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquid!",
+    imgUrl: "/images/project-thumb-demo.png",
+    sourceCodeUrl: "#",
+    projectUrl: "#",
+  },
+  {
+    id: "002",
+    projectName: "Project Name",
+    projectDescription:
+      "This is a front-end of a website built with raw HTML and CSS. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquid!",
+    imgUrl: "/images/project-thumb-demo.png",
+    sourceCodeUrl: "#",
+    projectUrl: "#",
+  },
+  {
+    id: "003",
+    projectName: "Project Name",
+    projectDescription:
+      "This is a front-end of a website built with raw HTML and CSS. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquid!",
+    imgUrl: "/images/project-thumb-demo.png",
+    sourceCodeUrl: "#",
+    projectUrl: "#",
+  },
+  {
+    id: "004",
+    projectName: "Project Name",
+    projectDescription:
+      "This is a front-end of a website built with raw HTML and CSS. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquid!",
+    imgUrl: "/images/project-thumb-demo.png",
+    sourceCodeUrl: "#",
+    projectUrl: "#",
+  },
+  {
+    id: "005",
+    projectName: "Project Name",
+    projectDescription:
+      "This is a front-end of a website built with raw HTML and CSS. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquid!",
+    imgUrl: "/images/project-thumb-demo.png",
+    sourceCodeUrl: "#",
+    projectUrl: "#",
+  },
+  {
+    id: "006",
+    projectName: "Project Name",
+    projectDescription:
+      "This is a front-end of a website built with raw HTML and CSS. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aliquid!",
+    imgUrl: "/images/project-thumb-demo.png",
+    sourceCodeUrl: "#",
+    projectUrl: "#",
+  },
+];
