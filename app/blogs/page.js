46cd937974c99e9863d22ca4345747201347f0fd
smsh119 <suzathoq16@gmail.com>
2025-12-09 23:59:08 +0600

    implemented projects and blogs page filtering functionality

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,15 +1,24 @@
 import Blogs from "@/components/Blogs/Blogs";
 import Filter from "@/components/Filter";
 import { getBlogs } from "@/lib/contents";
+import filterByCategoryAndSearch from "@/lib/filterByCategoryAndSearch";
 
 async function BlogsPage({ searchParams }) {
   const filters = await searchParams;
-  const blogs = getBlogs();
+  const allBlogs = getBlogs();
+  const categorySet = new Set();
+
+  allBlogs.forEach((blog) => {
+    blog?.categories?.forEach((cat) => categorySet.add(cat));
+  });
+
+  const blogs = filterByCategoryAndSearch(allBlogs, filters);
+
   return (
     <>
       <Filter
         searchPlaceholder="Search blog"
-        categories={["Tech", "Travel"]}
+        categories={[...categorySet]}
         url={`/blogs`}
         filters={filters}
       />
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,15 +1,24 @@
 import Filter from "@/components/Filter";
 import Projects from "@/components/Projects/Projects";
 import { getProjects } from "@/lib/contents";
+import filterByCategoryAndSearch from "@/lib/filterByCategoryAndSearch";
 
 async function ProjectsPage({ searchParams }) {
   const filters = await searchParams;
-  const projects = getProjects();
+  const allProjects = getProjects();
+  const categorySet = new Set();
+
+  allProjects.forEach((proj) => {
+    proj?.categories?.forEach((cat) => categorySet.add(cat));
+  });
+
+  const projects = filterByCategoryAndSearch(allProjects, filters);
+
   return (
     <>
       <Filter
         searchPlaceholder="Search project"
-        categories={["Web", "Mobile", "Desktop"]}
+        categories={[...categorySet]}
         url={`/projects`}
         filters={filters}
       />
