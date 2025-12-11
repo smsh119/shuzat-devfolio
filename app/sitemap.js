37a8d62fbe2df1342475ed1cfdb9975d43a041e1
smsh119 <suzathoq16@gmail.com>
2025-12-12 01:11:25 +0600

    implemented sitemap and refactored robots.txt file

---
+++ app/sitemap.js
@@ -0,0 +1,39 @@
+import { getBlogs, getProjects } from "@/lib/contents";
+
+export default function sitemap() {
+  const projectsSitemaps = getProjects().map((proj) => ({
+    url: `${process.env.SITE_URL}/projects/${proj.id}`,
+    lastModified: new Date(proj?.lastModified),
+    changeFrequency: "yearly",
+    priority: 0.8,
+  }));
+  const blogsSitemaps = getBlogs().map((blog) => ({
+    url: `${process.env.SITE_URL}/blogs/${blog.id}`,
+    lastModified: new Date(blog?.publishDate),
+    changeFrequency: "monthly",
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: `${process.env.SITE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${process.env.SITE_URL}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.SITE_URL}/blogs`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    ...projectsSitemaps,
+    ...blogsSitemaps,
+  ];
+}
