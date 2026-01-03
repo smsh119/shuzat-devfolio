2a71df0a7e98bc59e42d1f9e70f0610095c5fab8
smsh119 <suzathoq16@gmail.com>
2026-01-03 16:29:43 +0600

    updated nextjs version, set output to standalone for nextjs build in config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    mdxRs: false,
+  },
+};
 
 export default nextConfig;
